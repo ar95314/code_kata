e57145036d95c73cc3091c75d53a9e4f25e9f5e3
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-07-15 14:40:34 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,5 @@
+b=input("Enter the word: ")
+if(b =='A' or b == 'a' or b == 'E' or b == 'e' or b =='I' or b == 'i' or b == 'O' or b == 'o' or b == 'U' or b == 'u'):
+    print("Vowel")
+else:
+    print("Consonent")
