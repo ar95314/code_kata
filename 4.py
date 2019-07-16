7b42d74177b8825c2e6d5ce23f436aad95a70b22
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-07-16 12:26:02 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the number1:"))
+y=int(input("Enter the number2:"))
+z=int(input("Enter the number3:"))
+if(x>y):
+  if(x>z):
+    print(x)
+elif(y>z):
+  print(y)
+else:
+  print(z)
